0ca9abe31b7e3469955b7d91c27f508121c1f69d
hanamoradi <hanamoradiyy@gmail.com>
2026-06-06 13:25:39 +0430

    base structure

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className=''>
+        <div className='w-1/4 bg-[#100034] h-100 '>
+
+     </div>
+    </div>
+  )
+}
+
+export default Login
